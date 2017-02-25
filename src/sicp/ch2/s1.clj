fc12d9f5d2cda01af6504541c23ea56a771483b0
ajohendran <ajohendran@arideninc.com>
2017-02-25 06:57:31 -0600

    Completed Ex 2.14

--- src/sicp/ch2/s1.clj
+++ src/sicp/ch2/s1.clj
@@ -594,11 +594,8 @@
 ;; (def i1nn (make-interval -5.5 -4.5))
 ;; (def i2nn (make-interval -7.0 -5.0))
 
-;; (def i1pn (make-interval -5.5 4.5))
-;; (def i2pn (make-interval -7.0 5.0))
-
-;; (def i1np (make-interval -4.5 5.5))
-;; (def i2np (make-interval -5.0 7.0))
+;; (def i1np (make-interval -14.5 5.5))
+;; (def i2np (make-interval -15.0 7.0))
 
 
 ;; (defn show-muls [x y]
@@ -626,8 +623,8 @@
 
 
 
-;; (doseq [i1 [i1pp i1nn i1pn i1np]
-;;   i2 [i2pp i2nn i2pn i2np]
+;; (doseq [i1 [i1pp i1nn i1np]
+;;   i2 [i2pp i2nn i2np]
 ;;   proc [sub-interval]]
 ;;   (println (proc i1 i2))
 ;;   (println (show-subs i1 i2))
@@ -676,35 +673,37 @@
 ;; (-2.5 0.5)
 ;; widths: 0.5 1.0 1.5
 
-;; Add (4.5 5.5) to (-7.0 5.0)
-;; (-2.5 10.5)
-;; widths: 0.5 6.0 6.5
-
-;; Add (4.5 5.5) to (-5.0 7.0)
-;; (-0.5 12.5)
-;; widths: 0.5 6.0 6.5
+;; Add (4.5 5.5) to (-15.0 7.0)
+;; (-10.5 12.5)
+;; widths: 0.5 11.0 11.5
 
 ;; Add (-5.5 -4.5) to (5.0 7.0)
 ;; (-0.5 2.5)
 ;; widths: 0.5 1.0 1.5
 
-;; Add (-5.5 4.5) to (-7.0 5.0)
-;; (-12.5 9.5)
-;; widths: 5.0 6.0 11.0
+;; Add (-5.5 -4.5) to (-7.0 -5.0)
+;; (-12.5 -9.5)
+;; widths: 0.5 1.0 1.5
 
-;; Add (-5.5 4.5) to (-5.0 7.0)
-;; (-10.5 11.5)
-;; widths: 5.0 6.0 11.0
+;; Add (-5.5 -4.5) to (-15.0 7.0)
+;; (-20.5 2.5)
+;; widths: 0.5 11.0 11.5
 
-;; Add (-4.5 5.5) to (5.0 7.0)
-;; (0.5 12.5)
-;; widths: 5.0 1.0 6.0
-;; Add (-4.5 5.5) to (-7.0 -5.0)
+;; Add (-14.5 5.5) to (5.0 7.0)
+;; (-9.5 12.5)
+;; widths: 10.0 1.0 11.0
+
+;; Add (-14.5 5.5) to (-7.0 -5.0)
+;; (-21.5 0.5)
+;; widths: 10.0 1.0 11.0
+
+;; Add (-14.5 5.5) to (-15.0 7.0)
+;; (-29.5 12.5)
+;; widths: 10.0 11.0 21.0
 
 
 
 ;; Examples for Subtraction
-
 
 ;; Sub (5.0 7.0) from (4.5 5.5)
 ;; (-2.5 0.5)
@@ -714,13 +713,9 @@
 ;; (9.5 12.5)
 ;; widths: 0.5 1.0 1.5
 
-;; Sub (-7.0 5.0) from (4.5 5.5)
-;; (-0.5 12.5)
-;; widths: 0.5 6.0 6.5
-
-;; Sub (-5.0 7.0) from (4.5 5.5)
-;; (-2.5 10.5)
-;; widths: 0.5 6.0 6.5
+;; Sub (-15.0 7.0) from (4.5 5.5)
+;; (-2.5 20.5)
+;; widths: 0.5 11.0 11.5
 
 ;; Sub (5.0 7.0) from (-5.5 -4.5)
 ;; (-12.5 -9.5)
@@ -730,22 +725,21 @@
 ;; (-0.5 2.5)
 ;; widths: 0.5 1.0 1.5
 
-;; Sub (-7.0 5.0) from (-5.5 -4.5)
-;; (-10.5 2.5)
-;; widths: 0.5 6.0 6.5
+;; Sub (-15.0 7.0) from (-5.5 -4.5)
+;; (-12.5 10.5)
+;; widths: 0.5 11.0 11.5
 
-;; Sub (-7.0 -5.0) from (-4.5 5.5)
-;; (0.5 12.5)
-;; widths: 5.0 1.0 6.0
+;; Sub (5.0 7.0) from (-14.5 5.5)
+;; (-21.5 0.5)
+;; widths: 10.0 1.0 11.0
 
-;; Sub (-7.0 5.0) from (-4.5 5.5)
+;; Sub (-7.0 -5.0) from (-14.5 5.5)
 ;; (-9.5 12.5)
-;; widths: 5.0 6.0 11.0
+;; widths: 10.0 1.0 11.0
 
-;; Sub (-5.0 7.0) from (-4.5 5.5)
-;; (-11.5 10.5)
-;; widths: 5.0 6.0 11.0
-
+;; Sub (-15.0 7.0) from (-14.5 5.5)
+;; (-21.5 20.5)
+;; widths: 10.0 11.0 21.0
 
 
 
@@ -759,37 +753,33 @@
 ;; (-38.5 -22.5)
 ;; widths: 0.5 1.0 8.0
 
-;; Mul (4.5 5.5) with (-7.0 5.0)
-;; (-38.5 27.5)
-;; widths: 0.5 6.0 33.0
-
-;; Mul (4.5 5.5) with (-5.0 7.0)
-;; (-27.5 38.5)
-;; widths: 0.5 6.0 33.0
+;; Mul (4.5 5.5) with (-15.0 7.0)
+;; (-82.5 38.5)
+;; widths: 0.5 11.0 60.5
 
 ;; Mul (-5.5 -4.5) with (5.0 7.0)
 ;; (-38.5 -22.5)
 ;; widths: 0.5 1.0 8.0
 
-;; Mul (-5.5 4.5) with (-5.0 7.0)
-;; (-38.5 31.5)
-;; widths: 5.0 6.0 35.0
+;; Mul (-5.5 -4.5) with (-7.0 -5.0)
+;; (22.5 38.5)
+;; widths: 0.5 1.0 8.0
 
-;; Mul (-4.5 5.5) with (5.0 7.0)
-;; (-31.5 38.5)
-;; widths: 5.0 1.0 35.0
+;; Mul (-5.5 -4.5) with (-15.0 7.0)
+;; (-38.5 82.5)
+;; widths: 0.5 11.0 60.5
 
-;; Mul (-4.5 5.5) with (-7.0 -5.0)
-;; (-38.5 31.5)
-;; widths: 5.0 1.0 35.0
+;; Mul (-14.5 5.5) with (5.0 7.0)
+;; (-101.5 38.5)
+;; widths: 10.0 1.0 70.0
 
-;; Mul (-4.5 5.5) with (-7.0 5.0)
-;; (-38.5 31.5)
-;; widths: 5.0 6.0 35.0
+;; Mul (-14.5 5.5) with (-7.0 -5.0)
+;; (-38.5 101.5)
+;; widths: 10.0 1.0 70.0
 
-;; Mul (-4.5 5.5) with (-5.0 7.0)
-;; (-31.5 38.5)
-;; widths: 5.0 6.0 35.0
+;; Mul (-14.5 5.5) with (-15.0 7.0)
+;; (-101.5 217.5)
+;; widths: 10.0 11.0 159.5
 
 
 
@@ -805,26 +795,6 @@
 ;; (-1.1 -0.6428571428571428)
 ;; widths: 0.5 1.0 0.22857142857142865
 
-;; Div (4.5 5.5) by (5.0 7.0)
-;; Mul (4.5 5.5) with (0.14285714285714285 0.2)
-;; (0.6428571428571428 1.1)
-;; widths: 0.5 1.0 0.22857142857142865
-
-;; Div (4.5 5.5) by (-7.0 -5.0)
-;; Mul (4.5 5.5) with (-0.2 -0.14285714285714285)
-;; (-1.1 -0.6428571428571428)
-;; widths: 0.5 1.0 0.22857142857142865
-
-;; Div (4.5 5.5) by (-7.0 5.0)
-;; Mul (4.5 5.5) with (-0.14285714285714285 0.2)
-;; (-0.7857142857142857 1.1)
-;; widths: 0.5 6.0 0.9428571428571428
-
-;; Div (4.5 5.5) by (-5.0 7.0)
-;; Mul (4.5 5.5) with (-0.2 0.14285714285714285)
-;; (-1.1 0.7857142857142857)
-;; widths: 0.5 6.0 0.9428571428571428
-
 ;; Div (-5.5 -4.5) by (5.0 7.0)
 ;; Mul (-5.5 -4.5) with (0.14285714285714285 0.2)
 ;; (-1.1 -0.6428571428571428)
@@ -835,20 +805,20 @@
 ;; (0.6428571428571428 1.1)
 ;; widths: 0.5 1.0 0.22857142857142865
 
-;; Div (-4.5 5.5) by (5.0 7.0)
-;; Mul (-4.5 5.5) with (0.14285714285714285 0.2)
-;; (-0.9 1.1)
-;; widths: 5.0 1.0 1.0
+;; Div (-14.5 5.5) by (5.0 7.0)
+;; Mul (-14.5 5.5) with (0.14285714285714285 0.2)
+;; (-2.9000000000000004 1.1)
+;; widths: 10.0 1.0 2.0
 
-;; Div (-4.5 5.5) by (-7.0 -5.0)
-;; Mul (-4.5 5.5) with (-0.2 -0.14285714285714285)
-;; (-1.1 0.9)
-;; widths: 5.0 1.0 1.0
+;; Div (-14.5 5.5) by (-7.0 -5.0)
+;; Mul (-14.5 5.5) with (-0.2 -0.14285714285714285)
+;; (-1.1 2.9000000000000004)
+;; widths: 10.0 1.0 2.0
 
 
 
-;; (doseq [i1 [i1pp i1nn i1pn i1np]
-;;         i2 [i2pp i2nn i2pn i2np]
+;; (doseq [i1 [i1pp i1nn i1np]
+;;         i2 [i2pp i2nn i2np]
 ;;         proc [mul-interval]]
 ;;   (let [res (proc i1 i2)]
 ;;     (println res)
@@ -865,7 +835,6 @@
 
 (defn zero-spanning? [i]
   (and (neg? (lower-bound i)) (pos? (upper-bound i))))
-
 
 
 (defn div-interval [x y]
@@ -887,8 +856,8 @@
 ;;;;  Ex 2.11
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn mul-interval [x y]
-  (println "Mul" x "with" y)
+(defn mul-interval-old [x y]
+  (println "Mul Old" x "with" y)
   (let [p1 (* (lower-bound x) (lower-bound y))
         p2 (* (lower-bound x) (upper-bound y))
         p3 (* (upper-bound x) (lower-bound y))
@@ -897,25 +866,127 @@
                    (max p1 p2 p3 p4))))
 
 
-(defn )
+;; sicp.ch2.s1> [i1pp i1nn i1np]
+;; [(4.5 5.5) (-5.5 -4.5) (-14.5 5.5)]
+;; sicp.ch2.s1> [i2pp i2nn i2np]
+;; [(5.0 7.0) (-7.0 -5.0) (-15.0 7.0)]
+
 (defn mul-interval [x y]
+  (println "Mul" x "with" y)
   (let [xl (lower-bound x)
         xu (upper-bound x)
         yl (lower-bound y)
         yu (upper-bound y)]
     (cond
       (and (pos? xl) (pos? yl)) (make-interval (* xl yl) (* xu yu))
-      (and (neg? xu) (neg? yu)) (make-interval (* xl yl) (* xu yu))
-      (and (pos? xl) (neg? yu)) (make-interval (* xu yu) (* xl yl))
-      (and (neg? xu) (pos? yl)) (make-interval (* xu yu) (* xl yl))
-      )))
+      (and (pos? xl) (neg? yu)) (make-interval (* xu yl) (* xl yu))
+      (and (pos? xl) (zero-spanning? y)) (make-interval (* xu yl) (* xu yu))
+      (and (neg? xu) (pos? yl)) (make-interval (* xl yu) (* xu yl))
+      (and (neg? xu) (neg? yu)) (make-interval (* xu yu) (* xl yl))
+      (and (neg? xu) (zero-spanning? y)) (make-interval (* xl yu) (* xl yl))
+      (and (zero-spanning? x) (pos? yl)) (make-interval (* xl yu) (* xu yu))
+      (and (zero-spanning? x) (neg? yu)) (make-interval (* xu yl) (* xl yl))
+      (and (zero-spanning? x) (zero-spanning? y)) (make-interval (min (* xl yu) (* xu yl))
+                                                                 (max (* xl yl) (* xu yu))))))
 
+
+
+;; (doseq [i1 [i1pp i1nn i1np]
+;;         i2 [i2pp i2nn i2np]
+;;         proc [mul-interval-old mul-interval]]
+;;   (let [res (proc i1 i2)]
+;;     (println res)
+;;     (newline)))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  Ex 2.12
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn make-center-width [c w]
+  (make-interval (- c w) (+ c w)))
+
+(defn center-interval [i]
+  (/ (+ (lower-bound i) (upper-bound i)) 2.0))
+
+
+(defn width-interval [i]
+  (/ (- (upper-bound i) (lower-bound i)) 2.0))
+
+
+(defn make-center-percent  [c p]
+  (let [w (/ (* c p) 100.0)]
+    (make-center-width c w)))
+
+
+(defn percent-interval [i]
+  (let [w (width-interval i)]
+    (/ (* 100.0 w) (center-interval i))))
 
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  Ex 2.13
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(def ti1 (make-center-percent 10 1))
+(def ti2 (make-center-percent 50 2))
+
+
+;; sicp.ch2.s1> (percent-interval (mul-interval (make-center-percent 100 0.05)
+;;                                              (make-center-percent 5 0.05) ))
+;; 0.09999997500001193
+
+;; sicp.ch2.s1> (percent-interval (mul-interval (make-center-percent 100 0.05)
+;;                                              (make-center-percent 5 0.1) ))
+;; 0.14999992500003181
+
+;; sicp.ch2.s1> (percent-interval (mul-interval (make-center-percent 100 0.02)
+;;                                              (make-center-percent 5 0.5) ))
+;; 0.5199994800005245
+
+;; sicp.ch2.s1> (percent-interval (mul-interval (make-center-percent -100 0.02)
+;;                                              (make-center-percent 5 0.5) ))
+;; -0.5199994800005245
+
+;; sicp.ch2.s1> (percent-interval (mul-interval (make-center-percent -100 0.02)
+;;                                              (make-center-percent -5 0.5) ))
+;; 0.5199994800005245
+
+;; sicp.ch2.s1> (percent-interval (mul-interval (make-center-percent 100 0.02)
+;;                                              (make-center-percent -5 0.5) ))
+;; -0.5199994800005245
+
+
+;; From testing, it is clear that the percent tolerance of multiplication result is
+;; approximately the sum of individual percent tolerances
+
+
+;; Let X, Y be two centers. Let p1 and p2 be percentage tolerances.
+;; Let us assume the percentages p1 and p2 are expressed in decimal
+;; Interval X -> [X-(X*p1) , (X+(X*p1)]
+;; Can also be expressed as X ->  [X*(1-p1) , X*(1+p1)]
+;; Interval Y -> [Y-(Y*p2) , (Y+(Y*p2)]
+;; Can also be expressed as Y -> [Y*(1-p2) , Y*(1+p2)]
+
+;; Multiplication
+;; X * Y -> [ (X-(X*p1)) * (Y-(Y*p2)) , (X+(X*p1) * (Y+(Y*p2) ]
+;; [ X*(1-p1) * Y*(1-p2) , X*(1+p1) * Y*(1+p2)]
+;; [ X * Y * (1-p1)*(1-p2)  , X* Y * (1+p1)(1+p2) ]
+;; [ X*Y* (1-p2-p1+p1p2) , X*Y* (1+p2+p1+p1p2) ]
+;; If p1*p2 is small/negligible
+;; [ X*Y* (1-(p1+p2))  ,  X*Y* (1+(p1+p2)) ]
+
+;; So if p1*p2 is negligible, the tolerance for multiplication result is
+;; the sum of individual tolerances
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  Ex 2.14
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn par1 [r1 r2]
@@ -927,14 +998,191 @@
   (let [one (make-interval 1 1)] 
     (div-interval one
                   (add-interval (div-interval one r1)
-                                (div-interval one r2)))))
+                                (div-interval one r2))))))
 
+;; Using the resistor values given in the beginning of the exercise,
+;; (def r1 (make-center-percent 6.8 10.0))
+;; (def r2 (make-center-percent 4.7 5.0))
 
-;(def i1 (make-interval 6.12 7.48))
-;(def i2 (make-interval 4.465 4.935))
-
-
-;; (par1 i1 i2)
+;; (par1 r1 r2)
 ;; (2.201031010873943 3.4873689182805854)
-;; (par2 i1 i2)
+;; (par2 r1 r2)
 ;; (2.581558809636278 2.97332259363673)
+
+
+
+;; (def r1-mulr2-divr2 (div-interval (mul-interval r1 r2) r2))
+;; r1-mulr2-divr2
+;; (5.537142857142857 8.267368421052632)
+;; (center-interval r1-mulr2-divr2)
+;; 6.902255639097745
+;; (percent-interval r1-mulr2-divr2)
+;; 19.77777777777778
+;; (r1*r2)/r2 is algebraically quivalent to r1, but both center and tolerance are off
+
+
+;; (def A (make-center-percent 100 0.1))
+;; (def B (make-center-percent 50 0.2))
+
+;; (def A-div-A (div-interval A A))
+;; A-div-A
+;; (0.9980019980019981 1.002002002002002)
+;; (center-interval A-div-A)
+;; 1.000002000002
+;; (percent-interval A-div-A)
+;; 0.19999980000019435
+
+;; We can see what happened , based on what we learned from  Ex 2.13
+;; Div (99.9 100.1) by (99.9 100.1)
+;; is executed as
+;; Mul (99.9 100.1) with (0.00999000999000999 0.01001001001001001)
+;; As seen in exercise 2.13, the tolerances get added for multiplication
+;; Even though we would expect A/A to produce 1, the actual result
+;; is different and has a higher tolerance than initial starting point
+
+
+
+;; A
+;; (99.9 100.1)
+;; (percent-interval A)
+;; 0.09999999999999432
+;; (width-interval A)
+;; 0.09999999999999432
+
+;; B
+;; (49.9 50.1)
+;; (percent-interval B)
+;; 0.20000000000000284
+;; (width-interval B)
+;; 0.10000000000000142
+
+
+;; (def A-div-B (div-interval A B))
+;; A-div-B
+;; (1.9940119760479043 2.006012024048096)
+
+;; (center-interval A-div-B)
+;; 2.000012000048
+
+;; (percent-interval A-div-B)
+;; 0.2999994000011958
+
+;; Div (99.9 100.1) by (49.9 50.1)
+;; is executed as 
+;; Mul (99.9 100.1) with (0.01996007984031936 0.02004008016032064)
+;; the tolerances got added
+
+
+;; A
+;; (99.9 100.1)
+;; (percent-interval A)
+;; 0.09999999999999432
+;; (width-interval A)
+;; 0.09999999999999432
+
+;; (def A-add-A (add-interval A A))
+;; A-add-A
+;; (199.8 200.2)
+;; (center-interval A-add-A)
+;; 200.0
+;; (percent-interval A-add-A)
+;; 0.09999999999999432
+;; (width-interval A-add-A)
+;; 0.19999999999998863
+
+
+;; (def A-add-A-sub-A (sub-interval A-add-A A))
+;; A-add-A-sub-A
+;; (99.70000000000002 100.29999999999998)
+;; (center-interval A-add-A-sub-A)
+;; 100.0
+;; (percent-interval A-add-A-sub-A)
+;; 0.29999999999998295
+;; (width-interval A-add-A-sub-A)
+;; 0.29999999999998295
+
+;; We added A to A and subtracted A from that result.
+;; The end result has higher width and tolerance than A
+
+
+
+;; (A+B) * (A-B) is algebraically equivalent to A^2 - B^2
+
+;; (A+B) * (A-B) --> 150 * 50 --> 7500
+;; A^2 - B^2 --> 100^2 + 50^2 --> 10000 - 2500 = 7500
+
+;; A
+;; (99.9 100.1)
+;; (percent-interval A)
+;; 0.09999999999999432
+;; (width-interval A)
+;; 0.09999999999999432
+
+;; B
+;; (49.9 50.1)
+;; (percent-interval B)
+;; 0.20000000000000284
+;; (width-interval B)
+;; 0.10000000000000142
+
+;; (def f1 (mul-interval (add-interval A B)
+;;                       (sub-interval A B)))
+;; (def f2 (sub-interval (mul-interval A A)
+;;                       (mul-interval B B)))
+;; f1
+;; (7460.040000000001 7540.039999999999)
+;; f2
+;; (7470.000000000002 7529.999999999998)
+;; (center-interval f1)
+;; 7500.04
+;; (center-interval f2)
+;; 7500.0
+;; (percent-interval f1)
+;; 0.5333304889040471
+;; (percent-interval f2)
+;; 0.39999999999997576
+;; (width-interval f1)
+;; 39.99999999999909
+;; (width-interval f2)
+;; 29.99999999999818
+
+;; (def A-add-B (add-interval A B))
+;; A-add-B
+;; (149.8 150.2)
+;; (center-interval A-add-B)
+;; 150.0
+;; (percent-interval A-add-B)
+;; 0.13333333333332575
+;; (width-interval A-add-B)
+;; 0.19999999999998863
+
+;; (def A-sub-B (sub-interval A B))
+;; A-sub-B
+;; (49.800000000000004 50.199999999999996)
+;; (center-interval A-sub-B)
+;; 50.0
+;; (percent-interval A-sub-B)
+;; 0.3999999999999915
+;; (width-interval A-sub-B)
+;; 0.19999999999999574
+
+;; (def A-mul-A (mul-interval A A))
+;; A-mul-A
+;; (9980.010000000002 10020.009999999998)
+;; (center-interval A-mul-A)
+;; 10000.01
+;; (percent-interval A-mul-A)
+;; 0.1999998000001818
+;; (width-interval A-mul-A)
+;; 19.99999999999818
+
+
+;; (def B-mul-B (mul-interval B B))
+;; B-mul-B
+;; (2490.0099999999998 2510.01)
+;; (center-interval B-mul-B)
+;; 2500.01
+;; (percent-interval B-mul-B)
+;; 0.399998400006409
+;; (width-interval B-mul-B)
+;; 10.000000000000227
