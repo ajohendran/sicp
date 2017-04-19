367ea3e45b6d58fe59cf1a897311a728aaf9c154
Aravindh Johendran <ajohendran@arideninc.com>
2017-04-19 11:12:28 -0500

    Completed Ex 2.55

--- src/sicp/ch2/s3.clj
+++ src/sicp/ch2/s3.clj
@@ -38,5 +38,27 @@
 ;;;;  Ex 2.54
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; equality is a slippery slope with several possible interpretations
+;; and philosophical meanings. ALso there are several differencdes between
+;; clojure and scheme. So, this best be kept simple.
+
+;; this should be somewhat same as "=" in clojure
 (defn equal? [l1 l2]
-  (cond ())
+  (or (and (not (sequential? l1))
+           (not (sequential? l2))
+           (= l1 l2))
+      (and (sequential? l1)
+           (sequential? l2)
+           (= (first l1) (first l2))
+           (equal? (next l1) (next l2)))))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  Ex 2.55
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (car ''abracadabra)
+
+;; ''abracadabra is equivalent to (quote (quote abracadabra)
